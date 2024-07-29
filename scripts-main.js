728ed91f5bf977ad64bd3c531cc16be4caa77f62
bogachenko <bogachenkove@gmail.com>
2024-07-30 02:02:22 +0300

    Update index.html
    Correction in the code:
    * Code optimization.

--- scripts-main.js
+++ scripts-main.js
@@ -1,36 +1,41 @@
 // The script for collecting information about a user's IP and country.
-	fetch('https://api.ipify.org?format=json').then(response => response.json()).then(data =>
+fetch('https://api.ipify.org?format=json')
+	.then(response => response.json())
+	.then(data =>
 	{
 		const ipAddress = data.ip;
 		const infoContainer = document.getElementById('info-container');
 		infoContainer.innerHTML = `<p>Your IP address is: <strong>${ipAddress}</strong></p>`;
 		return fetch(`https://ipapi.co/${ipAddress}/json/`);
-	}).then(response => response.json()).then(data =>
+	})
+	.then(response => response.json())
+	.then(data =>
 	{
 		const country = data.country_name;
 		const infoContainer = document.getElementById('info-container');
-		return fetch('flags.xml').then(response => response.text()).then(xmlText =>
-		{
-			const parser = new DOMParser();
-			const xmlDoc = parser.parseFromString(xmlText, 'text/xml');
-			const flags = xmlDoc.getElementsByTagName('flag');
-			let flagBase64 = '';
-			for (let i = 0; i < flags.length; i++)
+		return fetch('flags.xml')
+			.then(response => response.text())
+			.then(xmlText =>
 			{
-				if (flags[i].getAttribute('country') === country)
+				const parser = new DOMParser();
+				const xmlDoc = parser.parseFromString(xmlText, 'text/xml');
+				const flags = xmlDoc.getElementsByTagName('flag');
+				let flagBase64 = '';
+				for(let i = 0; i < flags.length; i++)
 				{
-					flagBase64 = flags[i].getElementsByTagName('base64')[0].textContent;
-					break;
+					if(flags[i].getAttribute('country') === country)
+					{
+						flagBase64 = flags[i].getElementsByTagName('base64')[0].textContent;
+						break;
+					}
 				}
-			}
-			infoContainer.innerHTML += `<p>Your country is: <strong>${country}</strong> <img src="${flagBase64}" alt="${country} flag" class="flags"></p>`;
-		});
-	}).catch(error =>
+				infoContainer.innerHTML += `<p>Your country is: <strong>${country}</strong> <img src="${flagBase64}" alt="${country} flag" class="flags"></p>`;
+			});
+	})
+	.catch(error =>
 	{
 		console.error('Error fetching IP address or country:', error);
 		const infoContainer = document.getElementById('info-container');
-		infoContainer.innerHTML = `
-                <p>Your IP address is: <strong class="error-message">Unable to fetch IP address at this time.</strong></p><p>Your country is: <strong class="error-message">Unable to fetch country information at this time.</strong></p>
-            `;
+		infoContainer.innerHTML = `<p>Your IP address is: <strong class="error-message">Unable to fetch IP address at this time.</strong></p><p>Your country is: <strong class="error-message">Unable to fetch country information at this time.</strong></p>`;
 	});
 });
