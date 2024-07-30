a6358185876bf350ee41d91039c5678525c4892f
bogachenko <bogachenkove@gmail.com>
2024-07-31 00:46:16 +0300

    Update files
    Correction in the code:
    * Code optimization.

--- scripts-donate.js
+++ scripts-donate.js
@@ -1,3 +1,4 @@
+// The script for displaying wallet addresses for donations.
 function selectText(e) {
 	let t = document.createRange(),
 		n = window.getSelection();
@@ -12,8 +13,8 @@ document.addEventListener("DOMContentLoaded", function() {
 	fetch("json-donate.json").then(e => e.json()).then(a => {
 		let s = new Map,
 			d = a.qr_codes;
-		Object.keys(a.networks).forEach(e => {
-			Object.keys(a.networks[e]).forEach(t => {
+		Object.keys(a.chains).forEach(e => {
+			Object.keys(a.chains[e]).forEach(t => {
 				s.has(t) || s.set(t, []), s.get(t).push(e)
 			})
 		}), s.forEach((e, n) => {
@@ -21,20 +22,20 @@ document.addEventListener("DOMContentLoaded", function() {
 			o.value = n, o.textContent = n, t.appendChild(o)
 		}), t.addEventListener("change", function() {
 			let a = t.value;
-			if(a) {
-				e.innerHTML = '<option value="">Select Network</option>';
+			if (a) {
+				e.innerHTML = '<option value="">Choosing a chain</option>';
 				let d = s.get(a);
 				d.forEach(t => {
 					let n = document.createElement("option");
 					n.value = t, n.textContent = t, e.appendChild(n)
 				}), n.style.display = "none"
-			} else e.innerHTML = '<option value="">Select Network</option>', o.textContent = "", l.src = "", n.style.display = "none"
+			} else e.innerHTML = '<option value="">Choosing a chain</option>', o.textContent = "", l.src = "", n.style.display = "none"
 		}), e.addEventListener("change", function() {
 			let s = e.value,
 				r = t.value;
-			if(s && r) {
-				let c = a.networks[s][r];
-				if(c) {
+			if (s && r) {
+				let c = a.chains[s][r];
+				if (c) {
 					o.textContent = `${c.address}`;
 					let i = c.qrcode_address,
 						g = d[i];
--- scripts-main.js
+++ scripts-main.js
@@ -1,23 +1,23 @@
-// 
+// The script for displaying basic user data.
 document.addEventListener('DOMContentLoaded', function() {
 	function displayUserAgentAndOS() {
 		const userAgent = navigator.userAgent;
 		const infoContainer = document.getElementById('info-container');
 		infoContainer.innerHTML += '<p>User-Agent: <strong>' + userAgent + '</strong></p>';
 		let os = 'Unknown OS';
-		if(/Android/.test(userAgent)) os = 'Android';
-		else if(/iPhone/.test(userAgent)) os = 'iOS (iPhone)';
-		else if(/iPad/.test(userAgent)) os = 'iOS (iPad)';
-		else if(/iPod/.test(userAgent)) os = 'iOS (iPod)';
-		else if(/Macintosh/.test(userAgent)) os = 'Mac OS X';
-		else if(/Windows NT 10.0/.test(userAgent)) os = 'Windows 10';
-		else if(/Windows NT 6.3/.test(userAgent)) os = 'Windows 8.1';
-		else if(/Windows NT 6.2/.test(userAgent)) os = 'Windows 8';
-		else if(/Windows NT 6.1/.test(userAgent)) os = 'Windows 7';
-		else if(/Windows NT 6.0/.test(userAgent)) os = 'Windows Vista';
-		else if(/Windows NT 5.1/.test(userAgent)) os = 'Windows XP';
-		else if(/Linux/.test(userAgent)) os = 'Linux';
-		else if(/X11/.test(userAgent)) os = 'UNIX';
+		if (/Android/.test(userAgent)) os = 'Android';
+		else if (/iPhone/.test(userAgent)) os = 'iOS (iPhone)';
+		else if (/iPad/.test(userAgent)) os = 'iOS (iPad)';
+		else if (/iPod/.test(userAgent)) os = 'iOS (iPod)';
+		else if (/Macintosh/.test(userAgent)) os = 'Mac OS X';
+		else if (/Windows NT 10.0/.test(userAgent)) os = 'Windows 10';
+		else if (/Windows NT 6.3/.test(userAgent)) os = 'Windows 8.1';
+		else if (/Windows NT 6.2/.test(userAgent)) os = 'Windows 8';
+		else if (/Windows NT 6.1/.test(userAgent)) os = 'Windows 7';
+		else if (/Windows NT 6.0/.test(userAgent)) os = 'Windows Vista';
+		else if (/Windows NT 5.1/.test(userAgent)) os = 'Windows XP';
+		else if (/Linux/.test(userAgent)) os = 'Linux';
+		else if (/X11/.test(userAgent)) os = 'UNIX';
 		infoContainer.innerHTML += '<p>Operating System: <strong>' + os + '</strong></p>';
 	}
 	function fetchIpAndCountry() {
@@ -26,22 +26,22 @@ document.addEventListener('DOMContentLoaded', function() {
 			const infoContainer = document.getElementById('info-container');
 			infoContainer.innerHTML += `<p>Your IP address is: <strong>${ipAddress}</strong></p>`;
 			return fetch(`https://ipapi.co/${ipAddress}/json/`);
-		}).then(response => response.json()).then(data => {
+		}).then(response => response.json()).then(async data => {
 			const country = data.country_name;
 			const infoContainer = document.getElementById('info-container');
-			return fetch('flags.xml').then(response => response.text()).then(xmlText => {
-				const parser = new DOMParser();
-				const xmlDoc = parser.parseFromString(xmlText, 'text/xml');
-				const flags = xmlDoc.getElementsByTagName('flag');
-				let flagBase64 = '';
-				for(let i = 0; i < flags.length; i++) {
-					if(flags[i].getAttribute('country') === country) {
-						flagBase64 = flags[i].getElementsByTagName('base64')[0].textContent;
-						break;
-					}
+			const response = await fetch('flags.xml');
+			const xmlText = await response.text();
+			const parser = new DOMParser();
+			const xmlDoc = parser.parseFromString(xmlText, 'text/xml');
+			const flags = xmlDoc.getElementsByTagName('flag');
+			let flagBase64 = '';
+			for (let i = 0; i < flags.length; i++) {
+				if (flags[i].getAttribute('country') === country) {
+					flagBase64 = flags[i].getElementsByTagName('base64')[0].textContent;
+					break;
 				}
-				infoContainer.innerHTML += `<p>Country: <strong>${country}</strong> <img src="${flagBase64}" alt="${country} flag" class="flags"></p>`;
-			});
+			}
+			infoContainer.innerHTML += `<p>Country: <strong>${country}</strong> <img src="${flagBase64}" alt="${country} flag" class="flags"></p>`;
 		}).catch(error => {
 			console.error('Error fetching IP address or country:', error);
 			const infoContainer = document.getElementById('info-container');
