cd9b3bfb5cc65eb9e733e7a8d337aa7f67a374b8
bogachenko <bogachenkove@gmail.com>
2024-07-30 18:35:54 +0300

    Update files
    Correction in the code:
    * Small bugs fixed.
    * Code optimization.

--- scripts-donate.js
+++ scripts-donate.js
@@ -6,37 +6,37 @@ document.addEventListener("DOMContentLoaded", function() {
 		o = document.getElementById("address"),
 		l = document.getElementById("qr-code");
 	fetch("json-donate.json").then(e => e.json()).then(a => {
-		let d = new Map,
-			r = a.qr_codes;
+		let s = new Map,
+			d = a.qr_codes;
 		Object.keys(a.networks).forEach(e => {
 			Object.keys(a.networks[e]).forEach(t => {
-				d.has(t) || d.set(t, []), d.get(t).push(e)
+				s.has(t) || s.set(t, []), s.get(t).push(e)
 			})
-		}), d.forEach((e, n) => {
+		}), s.forEach((e, n) => {
 			let o = document.createElement("option");
 			o.value = n, o.textContent = n, t.appendChild(o)
 		}), t.addEventListener("change", function() {
-			let o = t.value;
-			if(o) {
+			let a = t.value;
+			if(a) {
 				e.innerHTML = '<option value="">Select Network</option>';
-				let l = d.get(o);
-				l.forEach(t => {
+				let d = s.get(a);
+				d.forEach(t => {
 					let n = document.createElement("option");
 					n.value = t, n.textContent = t, e.appendChild(n)
 				}), n.style.display = "none"
-			} else e.innerHTML = '<option value="">Select Network</option>'
+			} else e.innerHTML = '<option value="">Select Network</option>', o.textContent = "", l.src = "", n.style.display = "none"
 		}), e.addEventListener("change", function() {
-			let d = e.value,
-				s = t.value;
-			if(d && s) {
-				let i = a.networks[d][s];
+			let s = e.value,
+				r = t.value;
+			if(s && r) {
+				let i = a.networks[s][r];
 				if(i) {
 					o.textContent = `Wallet Address: ${i.address}`;
-					let c = i.qrcode_key,
-						E = r[c];
-					l.src = `data:image/svg+xml;base64,${E}`, n.style.display = "block"
+					let c = i.qr_code_key,
+						y = d[c];
+					l.src = `data:image/svg+xml;base64,${y}`, n.style.display = "block"
 				}
-			}
+			} else o.textContent = "", l.src = "", n.style.display = "none"
 		})
 	}).catch(e => console.error("Error loading data:", e))
 });
