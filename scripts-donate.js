501e0c6adf88a542d759a5dcad3fdd0ef655a2cb
bogachenko <bogachenkove@gmail.com>
2024-07-31 03:06:58 +0300

    Update donate.html
    Correction in the code:
    * Code optimization.
    * Minor bugs have been fixed.

--- scripts-donate.js
+++ scripts-donate.js
@@ -9,39 +9,38 @@ document.addEventListener("DOMContentLoaded", function() {
 		t = document.getElementById("coin"),
 		n = document.getElementById("result"),
 		o = document.getElementById("address"),
-		l = document.getElementById("qr-code");
-	fetch("json-donate.json").then(e => e.json()).then(a => {
-		let s = new Map,
-			d = a.qr_codes;
-		Object.keys(a.chains).forEach(e => {
-			Object.keys(a.chains[e]).forEach(t => {
-				s.has(t) || s.set(t, []), s.get(t).push(e)
+		s = document.getElementById("qr-code"),
+		a = document.getElementById("notification");
+	fetch("json-donate.json").then(e => e.json()).then(i => {
+		let l = new Map,
+			r = i.qr_codes;
+		Object.keys(i.chains).forEach(e => {
+			Object.keys(i.chains[e]).forEach(t => {
+				l.has(t) || l.set(t, []), l.get(t).push(e)
 			})
-		}), s.forEach((e, n) => {
+		}), l.forEach((e, n) => {
 			let o = document.createElement("option");
 			o.value = n, o.textContent = n, t.appendChild(o)
 		}), t.addEventListener("change", function() {
-			let a = t.value;
-			if (a) {
+			let i = t.value;
+			if(i) {
 				e.innerHTML = '<option value="">Choosing a chain</option>';
-				let d = s.get(a);
-				d.forEach(t => {
+				l.get(i).forEach(t => {
 					let n = document.createElement("option");
 					n.value = t, n.textContent = t, e.appendChild(n)
 				}), n.style.display = "none"
-			} else e.innerHTML = '<option value="">Choosing a chain</option>', o.textContent = "", l.src = "", n.style.display = "none"
+			} else e.innerHTML = '<option value="">Choosing a chain</option>', o.textContent = "", s.src = "", a.textContent = "", n.style.display = "none"
 		}), e.addEventListener("change", function() {
-			let s = e.value,
-				r = t.value;
-			if (s && r) {
-				let c = a.chains[s][r];
-				if (c) {
+			let l = e.value,
+				d = t.value;
+			if(l && d) {
+				let c = i.chains[l][d];
+				if(c) {
 					o.textContent = `${c.address}`;
-					let i = c.qrcode_address,
-						g = d[i];
-					l.src = `data:image/svg+xml;base64,${g}`, n.style.display = "block", selectText(o)
+					let h = r[c.qrcode_address];
+					s.src = `data:image/svg+xml;base64,${h}`, n.style.display = "block", selectText(o), a.innerHTML = `The coin <strong>${c.name}</strong> was chosen in the chain <strong>${l}</strong>. <strong><span class="warning-text">Please double-check the details.</span></strong><br><i>Make sure that the address <u>you have chosen</u> is used only for transferring the chosen <strong>${c.name}</strong> coin. Sending other coins to this address may result in the irreversible loss of assets.</i>`
 				}
-			} else o.textContent = "", l.src = "", n.style.display = "none"
+			} else o.textContent = "", s.src = "", a.textContent = "", n.style.display = "none"
 		})
 	}).catch(e => console.error("Error loading data:", e))
 });
